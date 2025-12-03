5464e670ebecb7a77d80f8310c7072d7b2536607
FastSameet <k250857@nu.edu.pk>
2025-12-03 23:12:47 +0500

    Create Q1.cpp

---
+++ lab10-11/Q1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    void setName(string n) {
+        name = n;
+    }
+    void setAge(int a) {
+        age = a;
+    }
+    string getName() {
+        return name;
+    }
+    int getAge() {
+        return age;
+    }
+};
+
+int main() {
+    Student s;       
+    s.setName("Ali"); 
+    s.setAge(20);    
+
+    cout << "Name: " << s.getName() << endl;
+    cout << "Age: " << s.getAge() << endl;
+
+    return 0;
+}
